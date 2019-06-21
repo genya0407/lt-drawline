284ead4c673db853becf1bb621a9fc71db57f4cc
Yusuke Sangenya <longinus.eva@gmail.com>
2019-06-21 14:27:24 +0900

    descriptioin

--- generate.rb
+++ generate.rb
@@ -1,7 +1,12 @@
 require "erb"
 
-@htmls = Dir.glob("boxes/*").map do |filename|
-  File.read(filename)
+Page = Struct.new(:title, :html)
+
+@pages = Dir["boxes/*"].sort.map do |filename|
+  page = Page.new
+  page.title = filename.match(%r[boxes/\d+_(.+).html])[1]
+  page.html = File.read(filename)
+  page
 end
 
 puts ERB.new(File.read("index.html.erb")).result(binding)
